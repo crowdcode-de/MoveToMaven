fd1da11767301d6e0214216ce165675d12c54cad
lofi <lofidewanto@gmail.com>
2013-01-08 14:16:44 +0100

    update CLI

--- movmvn-transformer-cli/src/main/bin/movmvn-cli.bat
+++ movmvn-transformer-cli/src/main/bin/movmvn-cli.bat
@@ -14,3 +14,55 @@ rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 rem See the License for the specific language governing permissions and
 rem limitations under the License.
 
+rem if "%MOVMVN_HOME%" == "" goto env_undefined
+if "%MOVMVN_HOME%" == "" set MOVMVN_HOME="..\movmvn-cli"
+
+:logo
+echo MoveToMaven Version 1.0.0-SNAPSHOT 
+echo.
+
+rem Java search
+if not "%JRE_HOME%" == "" goto gotJreHome
+if not "%JAVA_HOME%" == "" goto gotJavaHome
+echo No JAVA_HOME or JRE_HOME available.
+goto exit
+
+:gotJavaHome
+set "JRE_HOME=%JAVA_HOME%"
+
+:gotJreHome
+if not exist "%JRE_HOME%\bin\java.exe" goto noJreHome
+if not exist "%JRE_HOME%\bin\javaw.exe" goto noJreHome
+goto okJava
+
+:env_undefined
+echo Please define environment variable of MOVMVN_HOME.
+exit /b 32
+
+:noJreHome
+rem Needed at least a JRE
+echo Needed at least a JRE.
+goto exit
+
+:okJava
+rem Starter
+set PROGRAM_JAR=movmvn-transformer-cli-1.0.0-SNAPSHOT.jar
+set JAR_FILE=%MOVMVN_HOME%\lib\%PROGRAM_JAR%
+set _JAVA="%JRE_HOME%\bin\java"
+set HEAP_MS=-Xms512m
+set HEAP_MX=-Xmx1024m
+if "%JAVA_OPTS%" == "" set JAVA_OPTS=%HEAP_MS% %HEAP_MX%
+if "%LOGFILEPATH%" == "" set LOGFILEPATH=..\logs
+
+rem call Java
+%_JAVA% %JAVA_OPTS% -classpath %MOVMVN_HOME%\lib\* de.crowdcode.movmvn.cli.TransformerExecutor %1
+goto end
+
+:exit
+exit /b 32
+
+:end
+if not "%ERRORLEVEL%"=="" echo exit mit code %ERRORLEVEL% 
+exit /b %ERRORLEVEL%
+
+:exit
